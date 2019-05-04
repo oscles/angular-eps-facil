af673597b1f97ea57306135c396ac3b4c38ae3d1
oscles <osnaiderluis94@hotmail.com>
2019-05-04 17:41:31 -0500

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
+import {IndexComponent} from './containers/index/index.component';
+import {ContactoComponent} from './containers/contacto/contacto.component';
+import {FuncionesComponent} from './containers/funciones/funciones.component';
+import {GaleriaComponent} from './containers/galeria/galeria.component';
+import {ServicesComponent} from './containers/services/services.component';
+import {NoticiasComponent} from './containers/noticias/noticias.component';
+import {TimetableComponent} from './containers/timetable/timetable.component';
 
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'home', component: IndexComponent },
+  { path: 'servicios', component: ServicesComponent },
+  { path: 'features', component: FuncionesComponent },
+  { path: 'galeria', component: GaleriaComponent },
+  { path: 'timetable', component: TimetableComponent },
+  { path: 'noticias', component: NoticiasComponent },
+  { path: 'contacto', component: ContactoComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,25 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { IndexComponent } from './containers/index/index.component';
+import { ServicesComponent } from './containers/services/services.component';
+import { TimetableComponent } from './containers/timetable/timetable.component';
+import { NoticiasComponent } from './containers/noticias/noticias.component';
+import { GaleriaComponent } from './containers/galeria/galeria.component';
+import { ContactoComponent } from './containers/contacto/contacto.component';
+import { FuncionesComponent } from './containers/funciones/funciones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-    IndexComponent
+    IndexComponent,
+    ServicesComponent,
+    TimetableComponent,
+    NoticiasComponent,
+    GaleriaComponent,
+    ContactoComponent,
+    FuncionesComponent
   ],
   imports: [
     BrowserModule,
