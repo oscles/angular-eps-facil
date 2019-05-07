d5b215f80a315662ad5c68ce9f2d07a5a593f433
Oscles <32989503+oscles@users.noreply.github.com>
2019-05-07 12:52:30 -0500

    Update config.js

--- servidor/config/config.js
+++ servidor/config/config.js
@@ -1,6 +1,6 @@
 const SEED = 'clinic@oscles';
-const SECRET = 'XyOP9d4BJdHnXQ5HpipcyN5C';
-const CLIENT_ID = '1017405540216-o8snqcuoj8m0idjg48qlocjjn4s2g9dt.apps.googleusercontent.com';
+const SECRET = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
+const CLIENT_ID = 'xxxxxxxxxxxxxxxxxxxxxxxxx';
 
 CONFIG = {
     SEED,
